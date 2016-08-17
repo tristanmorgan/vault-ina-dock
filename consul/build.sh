6e1efb694b056459ee90b62dc3f725cc25494eaf
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2016-08-17 19:15:56 +1000

    add a version parameter to build scripts.

--- consul/build.sh
+++ consul/build.sh
@@ -2,6 +2,10 @@
 
 export CONSUL_VERSION=0.6.4
 
+if [ -n $1 ]; then
+  export CONSUL_VERSION=$1
+fi
+
 if [ ! -f consul_${CONSUL_VERSION}_linux_amd64.zip ]; then
   curl -OL https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_amd64.zip
 fi
--- vault/build.sh
+++ vault/build.sh
@@ -2,6 +2,10 @@
 
 export VAULT_VERSION=0.6.0
 
+if [ -n $1 ]; then
+  export VAULT_VERSION=$1
+fi
+
 if [ ! -f vault_${VAULT_VERSION}_linux_amd64.zip ]; then
   curl -OL https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
 fi
